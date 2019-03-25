45c8bed5dc6491d40106635cf900f662957ea508
许盛 <20691718@qq.com>
2019-03-25 23:43:56 +0800

    完善relink模块

--- src/relink/index.ts
+++ src/relink/index.ts
@@ -1,24 +1,29 @@
+import { join } from "path";
+import { homedir } from "os";
 import { exec } from "shelljs";
 import { appendFileSync } from "fs";
-import { pwd, time } from "./config.json";
+import { pwd } from "./config.json";
 
-const resultLog = "~/log/relink/result.log";
-const errorLog = "~/log/relink/error.log";
+const logDir = join(homedir(), "./log/relink");
+exec(`mkdir -p ${logDir}`);
+
+const resultLog = join(homedir(), "./log/relink/result.log");
+const errorLog = join(homedir(), "./log/relink/error.log");
 
 function format(str: string) {
-  return new Date() + " --- " + str;
+  return new Date() + " --- " + str + "\n";
 }
 
-setInterval(() => {
+(() => {
   let r = exec("ifconfig");
 
   if (r.code !== 0) {
     // ifconfig 命令出错
-    appendFileSync(errorLog, format(r.stderr));
+    appendFileSync(errorLog, format("ifconfig 出错：" + r.stderr));
     return;
   }
 
-  if (r.stdout.indexOf("192.168")) {
+  if (r.stdout.indexOf("192.168") !== -1) {
     // 网络正常
     appendFileSync(resultLog, format("ok"));
     return;
@@ -28,10 +33,10 @@ setInterval(() => {
 
   if (r.code !== 0) {
     //  重启出错
-    appendFileSync(errorLog, format(r.stderr));
+    appendFileSync(errorLog, format("重启出错：" + r.stderr));
     return;
   }
 
   // 重启成功
   appendFileSync(resultLog, "重启成功");
-}, 1000 * time);
+})();
